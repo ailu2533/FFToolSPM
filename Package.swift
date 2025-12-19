bf9c8b3a7029fb55c0cae0eb39ade8e1fda1e0b6
Your Name <your.email@example.com>
2025-12-19 23:21:38 +0800

    Remove deprecated FFmpegKit headers and Info.plist
    - Deleted internal muxer API header `fftools_ffmpeg_mux.h`.
    - Removed UTF-8 file opening utility header `fftools_fopen_utf8.h`.
    - Eliminated object pool header `fftools_objpool.h`.
    - Removed common option handlers header `fftools_opt_common.h`.
    - Deleted synchronization queue header `fftools_sync_queue.h`.
    - Removed thread queue header `fftools_thread_queue.h`.
    - Deleted Info.plist file from FFmpegKit framework.
    - Removed module map file `module.modulemap` for FFmpegKit framework.

--- Package.swift
+++ Package.swift
@@ -5,17 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "FFToolSPM",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FFToolSPM",
             targets: ["FFToolSPM"]),
     ],
+    dependencies: [
+             .package(url: "https://github.com/ailu2533/FFmpegKitSPM.git", branch: "main"),
+        ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(name: "FFmpegKit", path: "FFmpegKit.xcframework"),
         .target(
-            name: "FFToolSPM"),
+            name: "FFToolSPM",
+            dependencies: ["FFmpegKit"]),
 
     ]
 )
